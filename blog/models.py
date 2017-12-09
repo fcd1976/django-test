9e99db7637dc86efc67c6fb5e91c585c2acfd7ce
Fabiano Caixeta Duarte <fcd1976@gmail.com>
2017-12-09 20:00:19 -0200

    Fix on_delete

--- blog/models.py
+++ blog/models.py
@@ -4,7 +4,7 @@ from django.contrib.auth.models import User
 
 
 class Post(models.Model):
-    author = models.ForeignKey('auth.User')
+    author = models.ForeignKey('auth.User',on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
     text = models.TextField()
     created_date = models.DateTimeField(
